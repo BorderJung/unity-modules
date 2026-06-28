5771c4f3417f5658de77d035519e05233be3aa6b
BorderJung <jungse8609@gmail.com>
2026-06-29 04:24:45 +0900

    SaveLoad/Localization/Settings/UI 모듈 패키지 편입 (v1.1.0)
    - Dev~ → Runtime/Editor 편입, 네임스페이스 Border.<Module> 부여, .meta 동반/생성
      - Border.SaveLoad, Border.Localization, Border.Settings, Border.UI
      - Editor 코드(Localization 드로어/에디터) → 신규 Border.Editor asmdef
    - _Shared 중복(Log/Events)은 미편입 → 패키지의 Border.Core/Border.Events 로 수렴(using 주입)
    - Border.asmdef references += Unity.TextMeshPro, UnityEngine.UI
    - package.json dependencies += com.unity.ugui, version 1.1.0
    - Input(InputReader)은 생성 클래스 GameInput(.inputactions) 미수집으로 제외 → Dev~/Input 보관
    - 샘플 데이터(프리팹/SO/이벤트 인스턴스)는 코드만 편입하고 Dev~ 유지(GUID 보존)

--- Dev~/Input/InputReader.cs
+++ Dev~/Input/InputReader.cs
@@ -4,156 +4,160 @@ using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 
-[CreateAssetMenu(fileName = "InputReader", menuName = "Game/Input Reader")]
-public class InputReader : ScriptableObject, GameInput.IGameplayActions, GameInput.IMenusActions
+namespace Border.Input
 {
-    // GamePlay
-    public event UnityAction<Vector2> MoveEvent = delegate { };
-    public event UnityAction JumpEvent = delegate { };
-    public event UnityAction JumpCancelEvent = delegate { };
-
-    // Menus
-    public event UnityAction MenuPauseEvent = delegate { };
-    public event UnityAction MenuCloseEvent = delegate { };
-
-    private GameInput _gameInput;
-
-    private void OnEnable()
+    [CreateAssetMenu(fileName = "InputReader", menuName = "Game/Input Reader")]
+    public class InputReader : ScriptableObject, GameInput.IGameplayActions, GameInput.IMenusActions
     {
-        if (_gameInput == null)
-        {
-            _gameInput = new GameInput();
+        // GamePlay
+        public event UnityAction<Vector2> MoveEvent = delegate { };
+        public event UnityAction JumpEvent = delegate { };
+        public event UnityAction JumpCancelEvent = delegate { };
 
-            _gameInput.Gameplay.SetCallbacks(this);
-            _gameInput.Menus.SetCallbacks(this);
+        // Menus
+        public event UnityAction MenuPauseEvent = delegate { };
+        public event UnityAction MenuCloseEvent = delegate { };
+
+        private GameInput _gameInput;
+
+        private void OnEnable()
+        {
+            if (_gameInput == null)
+            {
+                _gameInput = new GameInput();
+
+                _gameInput.Gameplay.SetCallbacks(this);
+                _gameInput.Menus.SetCallbacks(this);
+                _gameInput.Gameplay.Enable();
+            }
+        }
+
+        private void OnDisable()
+        {
+            DisableAllInput();
+        }
+
+        /* Menus Actions */
+
+        public void OnMovement(InputAction.CallbackContext context)
+        {
+            MoveEvent.Invoke(context.ReadValue<Vector2>());
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            if (context.phase == InputActionPhase.Performed)
+                JumpEvent.Invoke();
+            else if (context.phase == InputActionPhase.Canceled)
+                JumpCancelEvent.Invoke();
+        }
+
+        public void OnAimArrow(InputAction.CallbackContext context)
+        {
+
+        }
+
+        public void OnAimMouse(InputAction.CallbackContext context)
+        {
+
+        }
+
+        public void OnRun(InputAction.CallbackContext context)
+        {
+
+        }
+
+        public void OnPull(InputAction.CallbackContext context)
+        {
+
+        }
+
+        public void OnPush(InputAction.CallbackContext context)
+        {
+
+        }
+
+        public void OnPause(InputAction.CallbackContext context)
+        {
+            if (context.phase == InputActionPhase.Performed)
+                MenuPauseEvent.Invoke();
+        }
+
+
+        /* Menus Actions */
+
+        public void OnCancel(InputAction.CallbackContext context)
+        {
+            if (context.phase == InputActionPhase.Performed)
+            {
+                MenuCloseEvent.Invoke();
+            }
+        }
+
+        public void OnMouseMove(InputAction.CallbackContext context)
+        {
+
+        }
+
+        public void OnMoveSelection(InputAction.CallbackContext context)
+        {
+
+        }
+
+        public void EnableGameplayInput()
+        {
+            _gameInput.Menus.Disable();
             _gameInput.Gameplay.Enable();
         }
-    }
 
-    private void OnDisable()
-    {
-        DisableAllInput();
-    }
-
-    /* Menus Actions */
-
-    public void OnMovement(InputAction.CallbackContext context)
-    {
-        MoveEvent.Invoke(context.ReadValue<Vector2>());
-    }
-
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        if (context.phase == InputActionPhase.Performed)
-            JumpEvent.Invoke();
-        else if (context.phase == InputActionPhase.Canceled)
-            JumpCancelEvent.Invoke();
-    }
-
-    public void OnAimArrow(InputAction.CallbackContext context)
-    {
-
-    }
-
-    public void OnAimMouse(InputAction.CallbackContext context)
-    {
-
-    }
-
-    public void OnRun(InputAction.CallbackContext context)
-    {
-
-    }
-
-    public void OnPull(InputAction.CallbackContext context)
-    {
-
-    }
-
-    public void OnPush(InputAction.CallbackContext context)
-    {
-
-    }
-
-    public void OnPause(InputAction.CallbackContext context)
-    {
-        if (context.phase == InputActionPhase.Performed)
-            MenuPauseEvent.Invoke();
-    }
-    
-
-    /* Menus Actions */
-
-    public void OnCancel(InputAction.CallbackContext context)
-    {
-        if (context.phase == InputActionPhase.Performed)
+        public void DisableGameplayInput()
         {
-            MenuCloseEvent.Invoke();
+            _gameInput.Gameplay.Disable();
+        }
+
+        public void EnableMenuInput()
+        {
+            _gameInput.Gameplay.Disable();
+            _gameInput.Menus.Enable();
+        }
+
+        public void DisableAllInput()
+        {
+            _gameInput.Gameplay.Disable();
+            _gameInput.Menus.Disable();
+        }
+
+        public bool GetGameplayInput()
+        {
+            return _gameInput.Gameplay.enabled;
+        }
+
+        public bool LeftMouseDown() => Mouse.current.leftButton.isPressed;
+
+        public void OnPoint(InputAction.CallbackContext context)
+        {
+
+        }
+
+        public void OnClick(InputAction.CallbackContext context)
+        {
+
+        }
+
+        public void OnRightClick(InputAction.CallbackContext context)
+        {
+
+        }
+
+        public void OnNavigate(InputAction.CallbackContext context)
+        {
+
+        }
+
+        public void OnSubmit(InputAction.CallbackContext context)
+        {
+
         }
     }
 
-    public void OnMouseMove(InputAction.CallbackContext context)
-    {
-
-    }
-
-    public void OnMoveSelection(InputAction.CallbackContext context)
-    {
-
-    }
-
-    public void EnableGameplayInput()
-    {
-        _gameInput.Menus.Disable();
-        _gameInput.Gameplay.Enable();
-    }
-
-    public void DisableGameplayInput()
-    {
-        _gameInput.Gameplay.Disable();
-    }
-
-    public void EnableMenuInput()
-    {
-        _gameInput.Gameplay.Disable();
-        _gameInput.Menus.Enable();
-    }
-
-    public void DisableAllInput()
-    {
-        _gameInput.Gameplay.Disable();
-        _gameInput.Menus.Disable();
-    }
-
-    public bool GetGameplayInput()
-    {
-        return _gameInput.Gameplay.enabled;
-    }
-
-    public bool LeftMouseDown() => Mouse.current.leftButton.isPressed;
-
-    public void OnPoint(InputAction.CallbackContext context)
-    {
-        
-    }
-
-    public void OnClick(InputAction.CallbackContext context)
-    {
-        
-    }
-
-    public void OnRightClick(InputAction.CallbackContext context)
-    {
-        
-    }
-
-    public void OnNavigate(InputAction.CallbackContext context)
-    {
-        
-    }
-
-    public void OnSubmit(InputAction.CallbackContext context)
-    {
-        
-    }
 }
